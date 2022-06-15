b0f424cee1d246adfca817476d01489b1f756b1f
João Pereira <joaopereira.eng.tec@gmail.com>
2022-06-15 16:08:05 +0100

    Registering a user

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
 
-const postgres = knex({
+const db = knex({
   client: "pg",
   connection: {
     host: "127.0.0.1",
@@ -13,7 +13,11 @@ const postgres = knex({
   },
 });
 
-//console.log(postgres.select("*").from("users"));
+db.select("*")
+  .from("users")
+  .then((data) => {
+    console.log(data);
+  });
 
 const app = express();
 app.use(express.json());
@@ -58,14 +62,17 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  database.users.push({
-    id: "125",
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date(),
-  });
-  res.json(database.users[database.users.length - 1]);
+  db("users")
+    .returning("*")
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date(),
+    })
+    .then((user) => {
+      res.json(user[0]);
+    })
+    .catch((err) => res.status(400).json(err));
 });
 
 app.get("/profile/:id", (req, res) => {
