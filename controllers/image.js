718eac59078258b573296040d4c262f3395539e4
João Pereira <joaopereira.eng.tec@gmail.com>
2022-06-21 16:14:17 +0100

    Security Review - Import Clarifai
    - Importing Clarifai from Frontend
    - Protecting apiKey

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "656398a1304c481c9411f48688d09f70",
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("Unable to work with API."));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db("users")
@@ -12,4 +27,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage,
+  handleApiCall,
 };
